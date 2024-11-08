521177fbcca323500937a7316283e0e5e4ff7e83
Romain Al <romainal@gmail.com>
2024-11-08 10:53:25 +0100

    New version before res2

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
-console.log(location.protocol);
-
-if (location.protocol !== 'https:') { // TODO
-    location.replace(`https:${location.href.substring(location.protocol.length)}`);
-    console.log('go https');
-    alert("Go HTTPS !");
-}
-
-const socket = io.connect("https://mypicoserv-unuojesj3q-od.a.run.app");
+// const socket = io.connect("http://192.168.10.2:1337");
+const socket = io.connect("https://pico-server-late-breeze-8245.fly.dev");
 socket.emit("join", "Pico", false);
-const v1 = document.getElementById('V1');
-const v2 = document.getElementById('V2');
-const v3 = document.getElementById('V3');
-const v4 = document.getElementById('V4');
-const v5 = document.getElementById('V5');
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate; 
 
-v1.onclick = ()=>{
-    let mess = "Vib1";
-    socket.emit("mess", mess);
-    console.log(mess);
-    if (navigator.vibrate){ navigator.vibrate(500); }
-}
-v2.onclick = ()=>{
-    let mess = "Vib2";
-    socket.emit("mess", mess);
-    console.log(mess);
-    if (navigator.vibrate){ navigator.vibrate(500); }
-}
-v3.onclick = ()=>{
-    let mess = "Vib3";
-    socket.emit("mess", mess);
-    console.log(mess);
-    if (navigator.vibrate){ navigator.vibrate(500); }
-}
-v4.onclick = ()=>{
-    let mess = "Vib4";
-    socket.emit("mess", mess);
-    console.log(mess);
-    if (navigator.vibrate){ navigator.vibrate(500); }
-}
-v5.onclick = ()=>{
-    let mess = "Vib5";
-    socket.emit("mess", mess);
-    console.log(mess);
-    if (navigator.vibrate){ navigator.vibrate(500); }
+const btns = document.getElementsByTagName("button");
+for (let i = 0; i<btns.length;i++){
+    btns[i].onclick = () => {
+        let mess = btns[i].getAttribute("id");
+        socket.emit("mess", mess);
+        console.log(mess);
+        if (navigator.vibrate){ navigator.vibrate(500); }
+    }
 }
