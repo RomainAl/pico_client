9fbd3f4d6c8e761e5f8038e4697083bcad7ff05b
Romain Al <romainal@gmail.com>
2024-12-10 16:59:43 +0100

    ok

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// const socket = io.connect("http://192.168.10.2:1337");
-const socket = io.connect("https://pico-server-late-breeze-8245.fly.dev");
+const socket = io.connect("http://192.168.10.2:1337");
+// const socket = io.connect("https://pico-server-late-breeze-8245.fly.dev");
 socket.emit("join", "Pico", false);
-navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate; 
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
 const btns = document.getElementsByTagName("button");
-for (let i = 0; i<btns.length;i++){
-    btns[i].onclick = () => {
-        let mess = btns[i].getAttribute("id");
-        socket.emit("mess", mess);
-        console.log(mess);
-        if (navigator.vibrate){ navigator.vibrate(500); }
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = () => {
+    let mess = btns[i].getAttribute("id");
+    socket.emit("mess", mess);
+    console.log(mess);
+    if (navigator.vibrate) {
+      navigator.vibrate(500);
     }
+  };
 }
